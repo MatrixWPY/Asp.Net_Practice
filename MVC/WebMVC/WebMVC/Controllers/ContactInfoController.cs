512b038cd6ac82c111c70e535d8cbc758d87e498
MatrixWPY <matrix.wpy@gmail.com>
2020-03-28 09:56:19 +0800

    [Update]WebMVC

--- MVC/WebMVC/WebMVC/Controllers/ContactInfoController.cs
+++ MVC/WebMVC/WebMVC/Controllers/ContactInfoController.cs
@@ -1,41 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using WebMVC.Models.Data;
-using Dapper;
-using System.Text;
+using WebMVC.Models.Repository;
 
 namespace WebMVC.Controllers
 {
     public class ContactInfoController : Controller
     {
-        private static readonly ConnectionStringSettings ConnectionSet = ConfigurationManager.ConnectionStrings["DBConnect"];
-
-        // GET: ContactInfo
         public ActionResult Index()
         {
-            List<ContactInfoData> liContactInfoData = GetContactInfo();
+            ContactInfoRepository objContactInfoRepository = new ContactInfoRepository();
+            List<ContactInfoData> liContactInfoData = objContactInfoRepository.GetContactInfo();
             return View(liContactInfoData);
         }
 
-        // GET: ContactInfo/Details/5
         public ActionResult Details(long ContactInfoID)
         {
-            ContactInfoData objContactInfoData = GetContactInfo(ContactInfoID);
+            ContactInfoRepository objContactInfoRepository = new ContactInfoRepository();
+            ContactInfoData objContactInfoData = objContactInfoRepository.GetContactInfo(ContactInfoID);
             return View(objContactInfoData);
         }
 
-        // GET: ContactInfo/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: ContactInfo/Create
         [HttpPost]
         public ActionResult Create(ContactInfoData objContactInfoData)
         {
@@ -44,7 +34,8 @@ namespace WebMVC.Controllers
                 // TODO: Add insert logic here
                 if (ModelState.IsValid)
                 {
-                    AddContactInfo(objContactInfoData);
+                    ContactInfoRepository objContactInfoRepository = new ContactInfoRepository();
+                    objContactInfoRepository.AddContactInfo(objContactInfoData);
                     return RedirectToAction("Index");
                 }
                 else
@@ -60,24 +51,24 @@ namespace WebMVC.Controllers
             }
         }
 
-        // GET: ContactInfo/Edit/5
         public ActionResult Edit(long ContactInfoID)
         {
-            ContactInfoData objContactInfoData = GetContactInfo(ContactInfoID);
+            ContactInfoRepository objContactInfoRepository = new ContactInfoRepository();
+            ContactInfoData objContactInfoData = objContactInfoRepository.GetContactInfo(ContactInfoID);
             return View(objContactInfoData);
         }
 
-        // POST: ContactInfo/Edit/5
         [HttpPost]
         public ActionResult Edit(long ContactInfoID, FormCollection form)
         {
             try
             {
                 // TODO: Add update logic here
-                ContactInfoData objContactInfoData = GetContactInfo(ContactInfoID);
+                ContactInfoRepository objContactInfoRepository = new ContactInfoRepository();
+                ContactInfoData objContactInfoData = objContactInfoRepository.GetContactInfo(ContactInfoID);
                 if (null != objContactInfoData && TryUpdateModel<ContactInfoData>(objContactInfoData, "", form.AllKeys, new string[] { "ContactInfoID", "CreateTime", "UpdateTime" }))
                 {
-                    UpdateContactInfo(objContactInfoData);
+                    objContactInfoRepository.UpdateContactInfo(objContactInfoData);
                     return RedirectToAction("Index");
                 }
                 else
@@ -93,24 +84,24 @@ namespace WebMVC.Controllers
             }
         }
 
-        // GET: ContactInfo/Delete/5
         public ActionResult Delete(long ContactInfoID)
         {
-            ContactInfoData objContactInfoData = GetContactInfo(ContactInfoID);
+            ContactInfoRepository objContactInfoRepository = new ContactInfoRepository();
+            ContactInfoData objContactInfoData = objContactInfoRepository.GetContactInfo(ContactInfoID);
             return View(objContactInfoData);
         }
 
-        // POST: ContactInfo/Delete/5
         [HttpPost]
         public ActionResult Delete(long ContactInfoID, FormCollection form)
         {
             try
             {
                 // TODO: Add delete logic here
-                ContactInfoData objContactInfoData = GetContactInfo(ContactInfoID);
+                ContactInfoRepository objContactInfoRepository = new ContactInfoRepository();
+                ContactInfoData objContactInfoData = objContactInfoRepository.GetContactInfo(ContactInfoID);
                 if (null != objContactInfoData)
                 {
-                    DeleteContactInfo(objContactInfoData);
+                    objContactInfoRepository.DeleteContactInfo(objContactInfoData);
                     return RedirectToAction("Index");
                 }
                 else
@@ -124,110 +115,6 @@ namespace WebMVC.Controllers
                 ModelState.AddModelError("DeleteError", "刪除失敗!");
                 return View();
             }
-        }
-
-        private List<ContactInfoData> GetContactInfo()
-        {
-            List<ContactInfoData> liContactInfoData = null;
-
-            using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
-            {
-                StringBuilder sbSQL = new StringBuilder();
-                sbSQL.AppendLine("select * from Tbl_ContactInfo");
-                sbSQL.AppendLine("where IsEnable=1");
-                sbSQL.AppendLine("order by ContactInfoID desc");
-
-                liContactInfoData = cn.Query<ContactInfoData>(sbSQL.ToString()).ToList();
-            }
-
-            return liContactInfoData;
-        }
-
-        private ContactInfoData GetContactInfo(long ContactInfoID)
-        {
-            ContactInfoData objContactInfoData = null;
-
-            using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
-            {
-                StringBuilder sbSQL = new StringBuilder();
-                sbSQL.AppendLine("select * from Tbl_ContactInfo");
-                sbSQL.AppendLine("where ContactInfoID=@ContactInfoID");
-                sbSQL.AppendLine("order by ContactInfoID desc");
-
-                objContactInfoData = cn.Query<ContactInfoData>(sbSQL.ToString(), new { ContactInfoID = ContactInfoID }).FirstOrDefault();
-            }
-
-            return objContactInfoData;
-        }
-
-        private bool AddContactInfo(ContactInfoData objContactInfoData)
-        {
-            bool bolResult = false;
-
-            try
-            {
-                using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
-                {
-                    objContactInfoData.IsEnable = true;
-                    objContactInfoData.CreateTime = DateTime.Now;
-
-                    long? ContactInfoID = cn.Insert(objContactInfoData);
-
-                    objContactInfoData.ContactInfoID = Convert.ToInt64(ContactInfoID);
-                }
-
-                bolResult = true;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            return bolResult;
-        }
-
-        private bool UpdateContactInfo(ContactInfoData objContactInfoData)
-        {
-            bool bolResult = false;
-
-            try
-            {
-                using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
-                {
-                    objContactInfoData.UpdateTime = DateTime.Now;
-
-                    cn.Update(objContactInfoData);
-                }
-
-                bolResult = true;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            return bolResult;
-        }
-
-        private bool DeleteContactInfo(ContactInfoData objContactInfoData)
-        {
-            bool bolResult = false;
-
-            try
-            {
-                using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
-                {
-                    cn.Delete(objContactInfoData);
-                }
-
-                bolResult = true;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            return bolResult;
         }
     }
 }
--- MVC/WebMVC/WebMVC/Models/Repository/ContactInfoRepository.cs
+++ MVC/WebMVC/WebMVC/Models/Repository/ContactInfoRepository.cs
@@ -1,11 +1,120 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
+using System.Text;
+using WebMVC.Models.Data;
 
 namespace WebMVC.Models.Repository
 {
     public class ContactInfoRepository
     {
+        private static readonly ConnectionStringSettings ConnectionSet = ConfigurationManager.ConnectionStrings["DBConnect"];
+
+        public List<ContactInfoData> GetContactInfo()
+        {
+            List<ContactInfoData> liContactInfoData = null;
+
+            using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
+            {
+                StringBuilder sbSQL = new StringBuilder();
+                sbSQL.AppendLine("select * from Tbl_ContactInfo");
+                sbSQL.AppendLine("where IsEnable=1");
+                sbSQL.AppendLine("order by ContactInfoID desc");
+
+                liContactInfoData = cn.Query<ContactInfoData>(sbSQL.ToString()).ToList();
+            }
+
+            return liContactInfoData;
+        }
+
+        public ContactInfoData GetContactInfo(long ContactInfoID)
+        {
+            ContactInfoData objContactInfoData = null;
+
+            using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
+            {
+                StringBuilder sbSQL = new StringBuilder();
+                sbSQL.AppendLine("select * from Tbl_ContactInfo");
+                sbSQL.AppendLine("where ContactInfoID=@ContactInfoID");
+                sbSQL.AppendLine("order by ContactInfoID desc");
+
+                objContactInfoData = cn.Query<ContactInfoData>(sbSQL.ToString(), new { ContactInfoID = ContactInfoID }).FirstOrDefault();
+            }
+
+            return objContactInfoData;
+        }
+
+        public bool AddContactInfo(ContactInfoData objContactInfoData)
+        {
+            bool bolResult = false;
+
+            try
+            {
+                using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
+                {
+                    objContactInfoData.IsEnable = true;
+                    objContactInfoData.CreateTime = DateTime.Now;
+
+                    long? ContactInfoID = cn.Insert(objContactInfoData);
+
+                    objContactInfoData.ContactInfoID = Convert.ToInt64(ContactInfoID);
+                }
+
+                bolResult = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return bolResult;
+        }
+
+        public bool UpdateContactInfo(ContactInfoData objContactInfoData)
+        {
+            bool bolResult = false;
+
+            try
+            {
+                using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
+                {
+                    objContactInfoData.UpdateTime = DateTime.Now;
+
+                    cn.Update(objContactInfoData);
+                }
+
+                bolResult = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return bolResult;
+        }
+
+        public bool DeleteContactInfo(ContactInfoData objContactInfoData)
+        {
+            bool bolResult = false;
+
+            try
+            {
+                using (var cn = new SqlConnection(ConnectionSet.ConnectionString))
+                {
+                    cn.Delete(objContactInfoData);
+                }
+
+                bolResult = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return bolResult;
+        }
     }
 }
