ed85ab9c653ea1f82ac22638aa31bf24a568e112
MatrixWPY <matrix.wpy@gmail.com>
2020-05-03 18:16:53 +0800

    [Add]WcfService

---
+++ WCF/WcfService/WcfService/IDBService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // 注意: 您可以使用 [重構] 功能表上的 [重新命名] 命令同時變更程式碼和組態檔中的介面名稱 "IService1"。
+    [ServiceContract]
+    public interface IDBService
+    {
+        // TODO: 在此新增您的服務作業
+    }
+}
