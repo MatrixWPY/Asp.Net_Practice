2a47259470bc50d6d423091ebffa43e57ef8e488
MatrixWPY <matrix.wpy@gmail.com>
2020-05-04 12:50:52 +0800

    [Update]WebAPI 1.WebApiConfig : ApiByAction Route Setting

--- API/WebAPI/WebAPI/App_Start/WebApiConfig.cs
+++ API/WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -14,16 +14,17 @@ namespace WebAPI
             // Web API 路由
             config.MapHttpAttributeRoutes();
 
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
-
             config.Routes.MapHttpRoute(
                 name: "ApiByAction",
                 routeTemplate: "api/{controller}/{action}",
-                defaults: new { action = "Post" }
+                defaults: new { action = "Post" },
+                constraints: new { controller = "ContactInfo" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
             );
         }
     }
